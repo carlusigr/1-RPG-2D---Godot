6ed2f09b3c4bd7f15b1334be982f607054c31607
Carlos Heduardo <carlusigr@hotmail.com>
2023-02-12 21:56:40 -0400

    Versao 003

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,9 +5,12 @@ const PARTICLES: PackedScene = preload("res://Scenes/Player/RunParticles.tscn")
 #Quando o Obj estiver pronto, guarda a referencia na Variavel
 onready var animation: AnimationPlayer = get_node("Animation")
 onready var sprite:  Sprite = get_node("Sprite")
+onready var colision: CollisionShape2D = get_node("AttackArea/Collision")
 
 #Variavel que carrega a posicao X e Y
 var velocity: Vector2
+var can_attack: bool = false
+var can_die: bool = false
 
 #Exporta a variavel para o Inspetor
 export (int) var speed
@@ -15,6 +18,7 @@ export (int) var speed
 #Funcao de Update (Verifica o tempo todo)
 func _physics_process(_delta: float) -> void:
 	move()
+	attack()
 	animate()
 	verify_direction()
 	
@@ -28,9 +32,19 @@ func move() -> void:
 	velocity = direction_vector * speed
 	velocity = move_and_slide(velocity)
 
+func attack() -> void:
+	if Input.is_action_just_pressed("ui_select") and not can_attack:
+		can_attack = true
+
 #Funcao feito para chamar as animacao do personagem
 func animate() -> void:
-	if velocity != Vector2.ZERO:
+	if can_die:
+		animation.play("Dead")
+		set_physics_process(false)
+	elif can_attack: 
+		animation.play("Attack")
+		set_physics_process(false)
+	elif velocity != Vector2.ZERO:
 		animation.play("Run")
 	else: 
 		animation.play("Idle")
@@ -39,9 +53,11 @@ func animate() -> void:
 func verify_direction() -> void:
 	if velocity.x > 0:
 		sprite.flip_h = false
+		colision.position = Vector2( 20, 8)
 		
 	elif velocity.x < 0:
 		sprite.flip_h = true
+		colision.position = Vector2( -20, 8)
 
 #Adicionando particulas de correr
 func instance_particles() -> void:
@@ -50,4 +66,14 @@ func instance_particles() -> void:
 	particle.global_position = global_position + Vector2(0, 16)
 	particle.play_particles()
 	
+func kill() -> void:
+	can_die = true
 	
+
+
+func on_animation_finished(anim_name):
+	if anim_name == "Dead":
+		var reload: bool = get_tree().reload_current_scene()
+	elif anim_name == "Attack":
+		can_attack = false
+		set_physics_process(true)
--- Scripts/Slime.gd
+++ Scripts/Slime.gd
@@ -3,6 +3,8 @@ extends KinematicBody2D
 onready var animation: AnimationPlayer = get_node("Animation")
 onready var sprite: Sprite = get_node("Sprite")
 
+var can_die: bool = false
+
 var player_ref = null
 var velocity: Vector2
 
@@ -19,6 +21,7 @@ func move() -> void:
 		var direction: Vector2 = distance.normalized()
 		var distance_length: float = distance.length()
 		if distance_length <= 5:
+			player_ref.kill()
 			velocity = Vector2.ZERO
 		else:
 			velocity = speed * direction
@@ -28,7 +31,10 @@ func move() -> void:
 	velocity = move_and_slide(velocity)	
 
 func animate() -> void:
-	if velocity != Vector2.ZERO:
+	if can_die:
+		animation.play("dead")
+		set_physics_process(false)
+	elif velocity != Vector2.ZERO:
 		animation.play("walk")
 	else:
 		animation.play("idle")
@@ -46,3 +52,14 @@ func on_body_entered(body):
 func on_body_exited(body):
 	if body.is_in_group("player"):
 		player_ref = null
+
+
+func kill(area):
+	if area.is_in_group("player_attack"):
+		can_die = true
+
+
+func on_animation_finished(anim_name):
+	if anim_name == "dead":
+		queue_free()
+
